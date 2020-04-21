066778a9951f1440fb1d6ea53a3f59c5ee879488
Julio Collado <jcollado@altimetrik.com>
2020-04-21 09:31:44 -0300

    Initial Commit

---
+++ Blur Effect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Blur Effect
+//
+//  Created by Julio Collado on 4/21/20.
+//  Copyright © 2020 julio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
